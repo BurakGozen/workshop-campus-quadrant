400f44badaaa040dd6194352f27a0d325acee00c
BurakGozen <info@newcode.be>
2023-05-25 08:34:10 +0200

    fix: navigation update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,4 @@
-import { exercises } from "@/config/exercises";
-import { Steps, Symbol } from "@newcodedevelopment/ui";
+import { Symbol } from "@newcodedevelopment/ui";
 import Link from "next/link";
 import { HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
@@ -22,18 +21,23 @@ export function Navigation({
       {...props}
       className={twMerge(
         "fixed left-0 top-0 z-[200] w-full transform transition-all duration-200 ease-in-out",
-        className
+        className,
       )}
     >
-      <div className="px-page bg-dark-700 flex flex-row items-center justify-between bg-opacity-20 py-4 backdrop-blur-xl backdrop-filter lg:bg-transparent lg:backdrop-blur-0 xl:py-12">
+      <div className="px-page flex flex-row items-center justify-between bg-opacity-20 py-4 xl:py-12">
         <Link href="/" className="z-10 flex flex-row items-center gap-3">
-          <Symbol className={twMerge("text-light-500 w-6  xl:w-12")} />
-          <span className="text-lg font-extrabold xl:text-3xl">NewCode</span>
+          <Symbol className={twMerge("text-light-500 w-6 xl:w-12")} />
+          <span className="text-md lg:text-lg font-extrabold xl:text-3xl">NewCode</span>
         </Link>
 
-        <Steps currentIndex={currentIndex} length={exercises.length} bgColor="light" />
+        {/* <Steps
+          className="w-20"
+          currentIndex={currentIndex}
+          length={exercises.length}
+          bgColor="light"
+        /> */}
 
-        <div className="hidden xl:flex xl:flex-row xl:gap-16">
+        <div className="flex flex-row gap-16">
           {paths.map(({ name, path, disabled }, index) =>
             disabled ? (
               <span
@@ -50,7 +54,7 @@ export function Navigation({
               >
                 {name}_
               </a>
-            )
+            ),
           )}
         </div>
       </div>
